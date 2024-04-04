2612d1609c7fd93885907da9c27e65bfba34d1aa
Lewis Liu <lewisl9029@users.noreply.github.com>
2024-04-04 10:35:07 -0700

    Update

--- packages/react/accessible-icon/src/AccessibleIcon.stories.tsx
+++ packages/react/accessible-icon/src/AccessibleIcon.stories.tsx
@@ -13,7 +13,7 @@ export const Styled = () => (
 
 export const Chromatic = () => (
   <p>
-    Some text with an inline accessible icon{' '}
+    Some text with an inline accessible icon oisfhois dfo s{' '}
     <AccessibleIcon label="Close">
       <CrossIcon />
     </AccessibleIcon>
